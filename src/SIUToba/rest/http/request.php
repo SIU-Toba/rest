8342b80891aa747578c252b1cca4853aa7e78c05
lleonardis <lleonardis@siu.edu.ar>
2022-07-27 15:36:26 -0300

    Se deshace mejora en los schemas para el Swagger.

--- src/SIUToba/rest/http/request.php
+++ src/SIUToba/rest/http/request.php
@@ -203,13 +203,7 @@ class request
     public function get_esquema()
     {
         if (!isset($this->protocol)) {
-            $isHttps =
-            (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
-            || (isset($_SERVER['REQUEST_SCHEME']) && $_SERVER['REQUEST_SCHEME'] === 'https')
-            || (isset($_SERVER['SERVER_PORT']) && (int) $_SERVER['SERVER_PORT'] === 443)
-            || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
-            ;
-            $this->protocol = ($isHttps) ? 'https' : 'http';
+            $this->protocol = (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off') ? 'http' : 'https';
         }
         return $this->protocol;
     }
